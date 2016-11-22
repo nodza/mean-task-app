902ebaa431924fd3481c03612b852d2d08f4a9b8
nodza <noel.hwande@gmail.com>
2016-11-22 10:17:37 -0600

    Added routes and actions for get and post.

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/meantodos', ['todos']);
 
-// GET TODOS
+// GET ALL TODOS
 router.get('/todos', function(req, res, next) {
     db.todos.find(function(err, todos) {
         if(err) {
@@ -13,6 +13,40 @@ router.get('/todos', function(req, res, next) {
         }
     })
 });
+
+// GET SINGLE TODO
+router.get('/todos/:id', function(req, res, next) {
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function(err, todo) {
+        if(err) {
+            res.send(err);
+        } else {
+            res.json(todo);
+        }
+    })
+});
+
+// POST A TODO
+router.post('/todo', function(req, res, next) {
+    var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + '')) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.save(todo, function(err, result) {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        })
+    }
+});
+
+// UPDATE A TODO
 
 
 
