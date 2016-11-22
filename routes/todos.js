681218bb5973095af891247826254f33cf510673
nodza <noel.hwande@gmail.com>
2016-11-22 10:43:58 -0600

    Added update and delete routes and methods.

--- routes/todos.js
+++ routes/todos.js
@@ -47,7 +47,47 @@ router.post('/todo', function(req, res, next) {
 });
 
 // UPDATE A TODO
+router.put('/todo/:id', function(req, res, next) {
+    var todo = req.body;
+    var updObj = {};
 
+    if(todo.isCompleted) {
+        updObj.isCompleted = todo.isCompleted;
+    }
 
+    if(todo.text) {
+        updObj.text = todo.text;
+    }
+
+    if(!updObj) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updObj, {}, function(err, result) {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
+});
+
+// DELETE A TODO
+router.delete('/todo/:id', function(req, res, next) {
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, '', function(err, result) {
+        if(err) {
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    })
+});
 
 module.exports = router;
