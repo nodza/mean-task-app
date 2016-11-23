24eceee40ef90f872a3011a31d81692c8bd5c516
nodza <noel.hwande@gmail.com>
2016-11-22 23:36:28 -0600

    Created client Angular app.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
